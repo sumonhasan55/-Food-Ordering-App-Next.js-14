d1b324f1c7f846a9a75f0c657e536f2449b0bfaf
sumonhasan55 <wubsumon7577@gmail.comm>
2023-12-13 00:57:00 +0600

    create hero section and footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from '@/components/layout/Header'
+import Hero from '@/components/layout/Hero'
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
   <>
- <Header/>
+
+ <Hero/>
   </>
   )
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,28 @@
-import Link from 'next/link'
 
-const Header = () => {
-    return (
-        <header className=' flex items-center justify-between'>
-            <Link className=' font-semibold text-primary ' href={''}>HM PIZZA</Link>
-            <nav className='flex items-center  gap-8 font-semibold text-gray-500 '>
-                <Link href={''}>Home</Link>
-                <Link href={''}>Contact</Link>
-                <Link href={''}>About</Link>
-                <Link href={''}>Menu</Link>
-                <Link className=' bg-primary text-white px-6 py-2 rounded-full' href={''}>Login</Link>
-            </nav>
-        </header>
-    );
-};
+import Link from "next/link";
 
-export default Header;
+export default function Header() {
+
+  return (
+    <header>
+ 
+  
+      <div className=" flex items-center justify-between">
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href={'/'}>
+            HM PIZZA
+          </Link>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/menu'}>Menu</Link>
+          <Link href={'/#about'}>About</Link>
+          <Link href={'/#contact'}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+            <Link href={"/login"}>Login</Link>
+            <Link className=" bg-primary text-white px-4 py-2 rounded-full" href={"/register"}>Register</Link>
+        </nav>
+       
+      </div>
+    </header>
+  );
+}
